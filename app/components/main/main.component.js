1bacc154e2fb13304fd59e858195a071383e1f74
elpaisa22 <sebastianecclesia@gmail.com>
2016-04-21 21:04:57 -0300

    Se modifica el look&feel por material2

--- app/components/main/main.component.js
+++ app/components/main/main.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'primeng/primeng'], function(exports_1, context_1) {
+System.register(['angular2/core', '@angular2-material/button/button', '@angular2-material/sidenav/sidenav', '@angular2-material/toolbar/toolbar', '@angular2-material/checkbox/checkbox', '@angular2-material/radio/radio', '@angular2-material/radio/radio_dispatcher', '@angular2-material/progress-circle/progress-circle', '@angular2-material/card/card', '@angular2-material/input/input', '@angular2-material/list/list', 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,17 +10,45 @@ System.register(['angular2/core', 'primeng/primeng'], function(exports_1, contex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, primeng_1, primeng_2, primeng_3;
+    var core_1, button_1, sidenav_1, toolbar_1, checkbox_1, radio_1, radio_dispatcher_1, progress_circle_1, card_1, input_1, list_1, router_1;
     var MainComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (primeng_1_1) {
-                primeng_1 = primeng_1_1;
-                primeng_2 = primeng_1_1;
-                primeng_3 = primeng_1_1;
+            function (button_1_1) {
+                button_1 = button_1_1;
+            },
+            function (sidenav_1_1) {
+                sidenav_1 = sidenav_1_1;
+            },
+            function (toolbar_1_1) {
+                toolbar_1 = toolbar_1_1;
+            },
+            function (checkbox_1_1) {
+                checkbox_1 = checkbox_1_1;
+            },
+            function (radio_1_1) {
+                radio_1 = radio_1_1;
+            },
+            function (radio_dispatcher_1_1) {
+                radio_dispatcher_1 = radio_dispatcher_1_1;
+            },
+            function (progress_circle_1_1) {
+                progress_circle_1 = progress_circle_1_1;
+            },
+            function (card_1_1) {
+                card_1 = card_1_1;
+            },
+            function (input_1_1) {
+                input_1 = input_1_1;
+            },
+            function (list_1_1) {
+                list_1 = list_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             MainComponent = (function () {
@@ -34,7 +62,19 @@ System.register(['angular2/core', 'primeng/primeng'], function(exports_1, contex
                     core_1.Component({
                         selector: 'main',
                         templateUrl: 'app/components/main/main.html',
-                        directives: [primeng_1.InputText, primeng_2.Button, primeng_3.Dialog]
+                        providers: [radio_dispatcher_1.MdRadioDispatcher],
+                        directives: [
+                            button_1.MdButton,
+                            sidenav_1.MD_SIDENAV_DIRECTIVES,
+                            toolbar_1.MdToolbar,
+                            checkbox_1.MdCheckbox,
+                            radio_1.MdRadioButton,
+                            progress_circle_1.MdSpinner,
+                            card_1.MD_CARD_DIRECTIVES,
+                            input_1.MD_INPUT_DIRECTIVES,
+                            list_1.MD_LIST_DIRECTIVES,
+                            router_1.ROUTER_DIRECTIVES
+                        ]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], MainComponent);
--- app/components/main/main.component.ts
+++ app/components/main/main.component.ts
@@ -1,14 +1,33 @@
 import {Component} from 'angular2/core';
 
-//primeng components
-import {InputText} from 'primeng/primeng';
-import {Button}    from 'primeng/primeng';
-import {Dialog}    from 'primeng/primeng';
+import {MdButton} from '@angular2-material/button/button';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav/sidenav';
+import {MdToolbar} from '@angular2-material/toolbar/toolbar';
+import {MdCheckbox} from '@angular2-material/checkbox/checkbox';
+import {MdRadioButton} from '@angular2-material/radio/radio';
+import {MdRadioDispatcher} from '@angular2-material/radio/radio_dispatcher';
+import {MdSpinner} from '@angular2-material/progress-circle/progress-circle';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card/card';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input/input';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list/list';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
 	selector: 'main',
-	templateUrl: 'app/components/main/main.html',
-    directives: [InputText, Button, Dialog]
+	templateUrl : 'app/components/main/main.html',
+	providers: [MdRadioDispatcher],
+  directives: [
+			          MdButton,
+								MD_SIDENAV_DIRECTIVES,
+							  MdToolbar,
+								MdCheckbox,
+								MdRadioButton,
+								MdSpinner,
+								MD_CARD_DIRECTIVES,
+								MD_INPUT_DIRECTIVES,
+								MD_LIST_DIRECTIVES,
+								ROUTER_DIRECTIVES
+							]
 })
 export class MainComponent {
     text: string;
