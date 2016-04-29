1514d4a869dc74baee6b909bb767223367b2a839
elpaisa22 <sebastianecclesia@gmail.com>
2016-04-29 14:44:32 -0300

    Mejora en el formato de fechas

--- app/services/cliente.service.js
+++ app/services/cliente.service.js
@@ -21,16 +21,16 @@ System.register(['angular2/core'], function(exports_1, context_1) {
             ClienteService = (function () {
                 function ClienteService() {
                     this.clientes = [
-                        { "id": 11, "nombre": "Mr. Nice", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "02134542", "fechaNacimiento": "31/11/2015" },
-                        { "id": 12, "nombre": "Narco", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "5464" },
-                        { "id": 13, "nombre": "Bombasto", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "654687" },
-                        { "id": 14, "nombre": "Celeritas", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "2135458" },
-                        { "id": 15, "nombre": "Magneta", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "985454" },
-                        { "id": 16, "nombre": "RubberMan", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "02134542" },
-                        { "id": 17, "nombre": "Dynama", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "5468787" },
-                        { "id": 18, "nombre": "Dr IQ", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "654878" },
-                        { "id": 19, "nombre": "Magma", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "897854" },
-                        { "id": 20, "nombre": "Tornado", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "2134545" }
+                        { "id": 11, "nombre": "Mr. Nice", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "02134542", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 12, "nombre": "Narco", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "5464", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 13, "nombre": "Bombasto", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "654687", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 14, "nombre": "Celeritas", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "2135458", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 15, "nombre": "Magneta", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "985454", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 16, "nombre": "RubberMan", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "02134542", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 17, "nombre": "Dynama", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "5468787", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 18, "nombre": "Dr IQ", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "654878", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 19, "nombre": "Magma", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "897854", "fechaNacimiento": new Date(1988, 3, 15) },
+                        { "id": 20, "nombre": "Tornado", "apellido": "Zapata", "email": "mrnice@gmail.com", "telefono": "2134545", "fechaNacimiento": new Date(1988, 3, 15) }
                     ];
                 }
                 ClienteService.prototype.getClientes = function () {
